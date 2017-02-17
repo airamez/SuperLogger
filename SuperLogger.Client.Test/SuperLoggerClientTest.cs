57f7665f72b0dc2200bf948071f54ab2714409f4
Jose Santos <airamez@gmail.com>
2017-02-17 00:48:40 -0800

    Initial Commit

---
+++ SuperLogger.Client.Test/SuperLoggerClientTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SuperLogger.Client.Test
+{
+    [TestClass]
+    public class SuperLoggerClientTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SuperLogger.Service/SuperLoggerService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperLogger.Service
+{
+    public partial class SuperLoggerService : ServiceBase
+    {
+        public SuperLoggerService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
